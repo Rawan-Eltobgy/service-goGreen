730623f70872bd01033e107b67bd0782ba42cd47
Rawan Eltobgy <rawan.eltobgy@harmonica.life>
2021-06-25 01:59:17 +0200

    feat: Add filter for data

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,13 +1,20 @@
 var vehicles = require('../vehicleList.json');
 var router = require('express').Router();
-//if calling /goGreen will add default page, limit to the url
-//Limit -> number of returned elements
-//startingIndex= (7* (pages -1)) 
-//
+
 router.get("/", (req, res) => {
-    const { page = 1, limit = 7 } = req.query
+    const { page = 1, limit = 7, filter } = req.query;
     let startingDataIndex = Number(limit) * (Number(page) - 1)
-        // console.log(startingDataIndex,startingRecipeIndex+limit,limit)
-    res.status(200).send(vehicles.slice(startingDataIndex, startingDataIndex + Number(limit)))
+    let vehicleDataResult = vehicles
+        //Enter the condition if filter not empty
+    if (filter) {
+        let filterData = filter.toLowerCase()
+        vehicleDataResult = vehicles.filter((vehicle) => {
+            console.log("vehicle is ", vehicle)
+            if (vehicle.brand.toLowerCase().includes(filterData) || vehicle.model.toLowerCase().includes(filterData)) {
+                return true;
+            }
+        })
+    }
+    res.status(200).send(vehicleDataResult.slice(startingDataIndex, startingDataIndex + Number(limit)))
 })
 module.exports = router;
